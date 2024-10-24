81e306b674e9b85044816055a9fc4ac16519effc
Tony <tony860725@gmail.com>
2024-10-24 22:14:28 +0800

    feat: add list node structure and arr2node func

---
+++ problems/206_Reverse_Linked_List/206_Reverse_Linked_List.go
@@ -0,0 +1,12 @@
+package problems
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+//func reverseList(head *structure.ListNode) *structure.ListNode {
+//
+//}
--- structure/list_node.go
+++ structure/list_node.go
@@ -4,3 +4,26 @@ type ListNode struct {
 	Val  int
 	Next *ListNode
 }
+
+func Arr2Node(input []int) *ListNode {
+	if len(input) == 0 {
+		return nil
+	}
+
+	head := &ListNode{
+		Val:  input[0],
+		Next: nil,
+	}
+	curr := head
+
+	for i := 1; i < len(input); i++ {
+		curr.Next = &ListNode{
+			Val:  input[i],
+			Next: nil,
+		}
+
+		curr = curr.Next
+	}
+
+	return head
+}
