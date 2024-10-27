02079f62b1fc22a11a3b36dad961752cb045d845
Tony <tony860725@gmail.com>
2024-10-27 15:13:09 +0800

    reslove 104 problem

--- problems/104_Maximum_Depth_of_Binary_Tree/104_Maximum_Depth_of_Binary_Tree.go
+++ problems/104_Maximum_Depth_of_Binary_Tree/104_Maximum_Depth_of_Binary_Tree.go
@@ -1,1 +1,18 @@
 package problems
+
+import "leet-code/structure"
+
+func maxDepth(root *structure.TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	leftDepth := maxDepth(root.Left)
+	rightDepth := maxDepth(root.Right)
+
+	if leftDepth > rightDepth {
+		return leftDepth + 1
+	}
+
+	return rightDepth + 1
+}
---
+++ structure/tree_node.go
@@ -0,0 +1,7 @@
+package structure
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
