f70ef5db490b7136922598d3d4f3881c6cec221f
Tony <tony860725@gmail.com>
2024-10-28 21:53:00 +0800

    reslove 100 problem

--- problems/100_Same_Tree/100_Same_Tree_test.go
+++ problems/100_Same_Tree/100_Same_Tree_test.go
@@ -1,1 +1,27 @@
 package problems
+
+import (
+	"leet-code/structure"
+	"testing"
+)
+
+func Test_isSameTree(t *testing.T) {
+	type args struct {
+		p *structure.TreeNode
+		q *structure.TreeNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSameTree(tt.args.p, tt.args.q); got != tt.want {
+				t.Errorf("isSameTree() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
