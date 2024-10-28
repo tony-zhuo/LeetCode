e563861f6a3369e49ee8db44acc3b812d4d598b3
Tony <tony860725@gmail.com>
2024-10-28 21:53:00 +0800

    reslove 100 problem

--- problems/100_Same_Tree/100_Same_Tree.go
+++ problems/100_Same_Tree/100_Same_Tree.go
@@ -1,1 +1,23 @@
 package problems
+
+import "leet-code/structure"
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSameTree(p *structure.TreeNode, q *structure.TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if (p != nil && q == nil) || (p == nil && q != nil) {
+		return false
+	} else if p.Val != q.Val {
+		return false
+	}
+
+	return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
