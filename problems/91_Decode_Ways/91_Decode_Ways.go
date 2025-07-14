6154b4d39c7f8d993b5eb1c11693dabf10e87a25
Tony Zhuo <tony860725@gmail.com>
2025-07-14 23:09:52 +0800

    solve 91 problem

---
+++ problems/91_Decode_Ways/91_Decode_Ways.go
@@ -0,0 +1,33 @@
+package problems
+
+func numDecodings(s string) int {
+	n := len(s)
+	if n == 0 {
+		return 0
+	}
+	//fmt.Println(int('0'))
+	// '0' = 48
+	// '1' = 49
+	// '2' = 50
+
+	dp := make([]int, n+1)
+	dp[0] = 1
+	if s[0] != '0' {
+		dp[1] = 1
+	} else {
+		dp[1] = 0
+	}
+
+	for i := 2; i <= n; i++ {
+		if s[i-1] != '0' {
+			dp[i] += dp[i-1]
+		}
+
+		two := (s[i-2]-'0')*10 + (s[i-1] - '0')
+		if two >= 10 && two <= 26 {
+			dp[i] += dp[i-2]
+		}
+	}
+
+	return dp[n]
+}
