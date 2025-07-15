856f41e6b1354880f4dd81a9f41b96730742e482
Tony Zhuo <tony860725@gmail.com>
2025-07-15 23:12:18 +0800

    solve ˇ322 problem

---
+++ problems/322_Coin_Change/322_Coin_Change.go
@@ -0,0 +1,22 @@
+package problems
+
+import "math"
+
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1) // dp[i]: 湊成金額 i 所需的 最少硬幣數
+	dp[0] = 0
+	for i := 1; i <= amount; i++ {
+		dp[i] = math.MaxInt32
+		for _, coin := range coins {
+			if i >= coin {
+				dp[i] = min(dp[i], dp[i-coin]+1)
+			}
+		}
+	}
+
+	if dp[amount] == math.MaxInt32 {
+		return -1
+	}
+
+	return dp[amount]
+}
