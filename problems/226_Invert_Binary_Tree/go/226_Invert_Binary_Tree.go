e76118bf82f5a03f4a6074e8ed75745abd611fef
Tony <tony860725@gmail.com>
2026-02-17 17:02:27 +0800

    feat: add Invert Binary Tree (#226) Go solution

--- problems/226_Invert_Binary_Tree/go/226_Invert_Binary_Tree.go
+++ problems/226_Invert_Binary_Tree/go/226_Invert_Binary_Tree.go
@@ -1,1 +1,15 @@
 package problems
+
+import datastructures "leet-code/data_structures/binary_tree/go"
+
+func invertTree(root *datastructures.TreeNode) *datastructures.TreeNode {
+	if root == nil {
+		return root
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+	root.Left = invertTree(root.Left)
+	root.Right = invertTree(root.Right)
+
+	return root
+}
