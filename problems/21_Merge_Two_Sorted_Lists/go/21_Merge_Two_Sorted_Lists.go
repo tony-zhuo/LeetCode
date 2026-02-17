fa65199c6680cbc27a4ce071f9ee1fbeaf5509e4
Tony <tony860725@gmail.com>
2026-02-17 11:47:16 +0800

    feat: add Merge Two Sorted Lists (#21) Go solution

--- problems/21_Merge_Two_Sorted_Lists/go/21_Merge_Two_Sorted_Lists.go
+++ problems/21_Merge_Two_Sorted_Lists/go/21_Merge_Two_Sorted_Lists.go
@@ -1,1 +1,28 @@
 package problems
+
+import datastructures "leet-code/data_structures/linked_list/go"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoLists(list1 *datastructures.ListNode, list2 *datastructures.ListNode) *datastructures.ListNode {
+	if list1 == nil {
+		return list2
+	}
+
+	if list2 == nil {
+		return list1
+	}
+
+	if list1.Val < list2.Val {
+		list1.Next = mergeTwoLists(list1.Next, list2)
+		return list1
+	} else {
+		list2.Next = mergeTwoLists(list1, list2.Next)
+		return list2
+	}
+}
