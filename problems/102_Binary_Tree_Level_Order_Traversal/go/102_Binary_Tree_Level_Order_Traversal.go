87225652723398e543816d22f3a6412214abe2fc
Tony <tony860725@gmail.com>
2026-03-05 23:04:42 +0800

    feat: add Binary Tree Level Order Traversal (#102) BFS solution and tests

--- problems/102_Binary_Tree_Level_Order_Traversal/go/102_Binary_Tree_Level_Order_Traversal.go
+++ problems/102_Binary_Tree_Level_Order_Traversal/go/102_Binary_Tree_Level_Order_Traversal.go
@@ -27,3 +27,36 @@ func scanTree(root *datastructures.TreeNode, level int, m map[int][]int) {
 
 	return
 }
+
+func levelOrder_BFS(root *datastructures.TreeNode) [][]int {
+	res := make([][]int, 0)
+
+	if root == nil {
+		return res
+	}
+
+	queue := make([]*datastructures.TreeNode, 0)
+	queue = append(queue, root)
+
+	for len(queue) != 0 {
+		n := len(queue)
+		level := []int{}
+		for i := 0; i < n; i++ {
+			node := queue[0]
+			queue = queue[1:]
+
+			level = append(level, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+
+		}
+
+		res = append(res, level)
+	}
+
+	return res
+}
--- problems/102_Binary_Tree_Level_Order_Traversal/go/102_Binary_Tree_Level_Order_Traversal_test.go
+++ problems/102_Binary_Tree_Level_Order_Traversal/go/102_Binary_Tree_Level_Order_Traversal_test.go
@@ -1,55 +1,61 @@
 package problems
 
 import (
-	"leet-code/libs"
 	datastructures "leet-code/data_structures/binary_tree/go"
+	"leet-code/libs"
 	"reflect"
 	"testing"
 )
 
+var cases = []struct {
+	name string
+	root []*int
+	want [][]int
+}{
+	{
+		name: "example 1",
+		root: []*int{libs.IntPtr(3), libs.IntPtr(9), libs.IntPtr(20), nil, nil, libs.IntPtr(15), libs.IntPtr(7)},
+		want: [][]int{
+			{3},
+			{9, 20},
+			{15, 7},
+		},
+	},
+	{
+		name: "single node",
+		root: []*int{libs.IntPtr(1)},
+		want: [][]int{
+			{1},
+		},
+	},
+	{
+		name: "empty tree",
+		root: []*int{},
+		want: nil,
+	},
+}
+
 func Test_levelOrder(t *testing.T) {
-	type args struct {
-		root []*int
-	}
-	tests := []struct {
-		name string
-		args args
-		want [][]int
-	}{
-		{
-			name: "",
-			args: args{
-				root: []*int{libs.IntPtr(3), libs.IntPtr(9), libs.IntPtr(20), nil, nil, libs.IntPtr(15), libs.IntPtr(7)},
-			},
-			want: [][]int{
-				{3},
-				{9, 20},
-				{15, 17},
-			},
-		},
-		{
-			name: "",
-			args: args{
-				root: []*int{libs.IntPtr(1)},
-			},
-			want: [][]int{
-				{1},
-			},
-		},
-		{
-			name: "",
-			args: args{
-				root: []*int{},
-			},
-			want: nil,
-		},
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
+	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			tree := datastructures.Slice2BinaryTree(tt.args.root)
+			tree := datastructures.Slice2BinaryTree(tt.root)
 			if got := levelOrder(tree); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("levelOrder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_levelOrder_BFS(t *testing.T) {
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			tree := datastructures.Slice2BinaryTree(tt.root)
+			got := levelOrder_BFS(tree)
+			if len(got) == 0 && len(tt.want) == 0 {
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("levelOrder_BFS() = %v, want %v", got, tt.want)
 			}
 		})
 	}
